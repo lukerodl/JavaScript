ccc510c00582be29a4ce36061c893dd0588eca80
lukerodl <lucasazrodrigues10@gmail.com>
2024-04-07 20:59:59 -0300

    aula de funções

---
+++ JavaScript/aula3.js
@@ -0,0 +1,80 @@
+/*var nomeInserido = prompt("Insira seu nome")
+var valorString1 = prompt("insira sua idade abaixo")
+var valornum1 = parseInt(valorString1)
+
+if (valornum1 >= 18) {
+    console.log(nomeInserido + " " + "é maior de idade");
+}
+
+var umNumero = 5
+
+
+// Comparando se umNumero é igual a 6
+if (umNumero == 6) {
+    console.log("não verá esta mensagem");
+} 
+
+var nomeUsuario = prompt("Inserir nome de usuário");
+
+if (nomeUsuario == "") {
+    alert("O nome de usuário não foi inserido");
+}
+else {
+    alert("Nome de usuário inserido: " + nomeUsuario);
+}  
+
+var combustivel = prompt("qual é o tipo de combustivel?");
+
+if (combustivel == "GNV") {
+    alert("O carro é movido à Gás");
+}
+else if (combustivel == "Gasolina") {
+    alert("O carro é movido à Gasolina");
+}
+else if (combustivel == "Álcool") {
+    alert("O carro é movido à Álcool");
+}
+else {
+    alert("O carro não funciona");
+}
+
+var number = prompt("Escreve um número de 1 a 5")
+
+if (number == 1) {
+    alert("número um")
+}
+
+else if (number == 2) {
+    alert("número dois")
+}
+
+else if (number == 3) {
+    alert("número três")
+}
+
+else if (number == 4) {
+    alert("número quatro")
+}
+
+else if (number == 5) {
+    alert("número cinco")
+}
+
+else {
+    alert("o número não é de 1 a 5")
+}
+
+var number = prompt("Insira um número de 1 a 30");
+
+if (number <= 10 && number >= 1) {
+    alert("seu número está entre 1 e 10")
+}
+else if (number >= 11 && number <= 20) {
+    alert("seu número está entre 10 e 20")
+}
+else if (number >= 21 && number <= 30) {
+    alert("seu número está entre 21 e 30")
+}
+else {
+    alert("seu número não está dentro da escala de 1 a 30")
+}*/
---
+++ JavaScript/aula4.js
@@ -0,0 +1,88 @@
+/*var numberString = prompt("Insira um número abaixo")
+var realnumber = parseInt(numberString)
+
+if(realnumber % 2 === 0)
+alert("O número " + realnumber + " é par")
+else {
+    alert("o número " + realnumber + " é impar")
+}
+
+function number() {
+    alert(realnumber)
+}
+
+number ()
+number ()
+number ()
+
+function name(nome) {
+    console.log(nome)
+}
+
+name("Lucas Leite")
+name("Luan Leite")
+
+
+//Declaração de variável para armazenar o resultado da soma
+var resultado = 0;
+
+//Função que soma dois números e atribui o resultado
+function somar(primeiroNumero, segundoNumero) {
+    resultado = primeiroNumero + segundoNumero
+}
+
+//Função que mostra o resultado no console
+function mostrar(mensagem) {
+    console.log(mensagem)
+}
+
+//Chamamos primeiro para somar e depois para mostrar
+somar(6, 3);
+    mostrar(resultado);
+
+    function somar(num1, num2) {
+        return num1 + num2;
+    }
+    var resultado = somar(5,8)
+    console.log(resultado)
+
+
+    function calculadora(primeiroNumero, segundoNumero, operacao) {
+        if (operacao == "+") {
+            return primeiroNumero + segundoNumero;
+        }
+        else if (operacao == "-") {
+            return primeiroNumero - segundoNumero;
+        }
+        else if (operacao == "*") {
+            return primeiroNumero * segundoNumero;
+        }
+        else if (operacao == "/") {
+            return primeiroNumero / segundoNumero;
+        }
+        else {
+            return 0;
+        }
+    }
+
+    var n1  = prompt("insira o primeiro número")
+    var n2  = prompt("insira o segundo número")
+    var n3  = prompt("insira a operação")
+    alert(calculadora(n1, n2, n3))
+
+
+    function subtrair(num1, num2) {
+        let resultado;
+        if(num1 > num2) {
+            resultado = num1 - num2;
+            console.log("o primeiro digito é maior que o segundo")
+        }
+        else {
+            resultado = num2 - num1;
+            console.log("o segundo é maior que o primeiro")
+        }
+    }
+
+    let numero1 = prompt("digite um número")
+    let numero2 = prompt("digite um número novamente")
+    subtrair(numero1, numero2);*/ 
