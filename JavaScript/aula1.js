f0b31595d90971f0d33842f62e09b2a30032ecc6
lukerodl <lucasazrodrigues10@gmail.com>
2024-04-07 02:41:20 -0300

    aula 1 JS

---
+++ JavaScript/aula1.js
@@ -0,0 +1,27 @@
+/*var nome = "Lucas"
+var sobrenome = "Leite"
+var anoAtual = 2024
+var idade = 23
+
+var respostaA = nome + sobrenome
+var respostaB = anoAtual - idade
+
+console.log("Nome completo: " + respostaA);
+console.log("Ano de nascimento: " + respostaB);
+
+
+    var nomeInserido = prompt("Insira seu nome");
+    alert("Obrigado pela resposta");
+
+
+    var valorString = prompt("Insira um número");
+    var valorNumerico = parseInt(valorString); 
+
+
+    var valorString1 = prompt("insira o primeiro número")
+    var valornum1 = parseInt(valorString1)
+    var valorString2 = prompt("insira o segundo número")
+    var valornum2 = parseInt(valorString2)
+
+    console.log(valornum1 + valornum2)
+    alert(valornum1 + valornum2)*/
