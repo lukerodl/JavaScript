762ac6d3e6fcca53da085e00f04eb34629ca8995
Lucas Leite <lucasazrodrigues10@gmail.com>
2024-07-17 19:03:03 -0300

    controle de fluxo

---
+++ Controle de Fluxo/index.js
@@ -0,0 +1,75 @@
+///////////////////////////////////////////////////1. CONDICIONAL
+
+/*var nomeUsuario = prompt("escreva seu nome abaixo")
+if (nomeUsuario === "") {
+    alert("O nome não pode ficar em branco")
+    prompt("Insira seu nome abaixo")
+} else {
+    alert("obrigado")
+    console.log(nomeUsuario)
+} */
+
+
+/*var combustivel = "Gasolina"
+if (prompt("Qual é o tipo de combustível") === combustivel) {
+    alert("Você mandou muito bem, é isso mesmo. Gasolina")
+} else { 
+    alert("errou, a resposta não é essa")
+}*/
+
+/*
+var combustível = "xyz"
+
+if(combustível === "gas"){
+    alert("O carro é movido a gas")
+} else if (combustível === "água"){
+    alert("O carro é movido a água")
+} else if (combustível === "alcool"){
+    alert("o carro é movido a alcool")
+} else {
+    alert("combustível não reconhecido")
+}*/
+
+//ATIVIDADE PRÁTICA 1
+/*
+var numero = prompt("escreva um número")
+var converçãonumero = parseFloat(numero)
+
+if (converçãonumero === 1) {
+    alert("Número um")
+} else if (converçãonumero === 2) {
+    alert("Número 2")
+} else if (converçãonumero === 3) {
+    alert("Número três")
+} else {
+    alert("O número selecionado não está no DB")
+} */
+
+
+    //////////////////////////////////////////////// 2. OPERADORES EM JS
+/*var nome = prompt("insira um nome")
+var sobrenome = prompt("insira um sobrenome")
+if ((nome != "") && (sobrenome != "")) {
+    alert(nome + " " + sobrenome)
+} else {
+    alert("insira um nome e sobrenome")
+}*/
+
+/*
+var nome = prompt("digite um nome")
+if((nome === "Ana") || (nome === "ana")) {
+    alert("obrigado")
+} else {
+    alert("nome não encontrado")
+}*/
+
+/*
+//ATIVIDADE PRÁTICA 2
+var letra = prompt("insira uma letra")
+if (letra === "") {
+    alert("você não inseriu uma letra")
+} else if ((letra === "a") || (letra === "e") || (letra === "i") || (letra === "o") || (letra === "u")) {
+    alert("sua letra é uma vogal")
+} else {
+    alert("sua letra não é vogal")
+}*/
