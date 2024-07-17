a910017e886cdc1179c742d22b0793a9d1481a52
Lucas Leite <lucasazrodrigues10@gmail.com>
2024-07-17 14:06:50 -0300

    Conceitos básicos JS

---
+++ Conceitos básicos JS/index.js
@@ -0,0 +1,86 @@
+/////////////////////////////////////////////////////////////2. TIPOS DE VARIÁVEIS E VALORES
+//declaração
+var nome
+var sobrenome
+var idade
+
+//atribuição
+nome = "Lucas" //string
+sobrenome = "Leite" //string
+idade = 23 //inteiros
+//Alguns casos que já se sabe o valor da variável, pode colocar: var nome = "Lucas"
+
+
+/////////////////////////////////////////////////////////////3. OPERADORES MATEMÁTICAS: 
+var primeiroNumero = 1
+var segundoNumero = 2
+var terceiroNumero = 3
+
+var soma = primeiroNumero + segundoNumero
+var subtração = segundoNumero - primeiroNumero
+var multiplicação = segundoNumero * terceiroNumero
+var potenciação = segundoNumero ** segundoNumero
+console.log("potenciação", potenciação)
+console.log(multiplicação)
+console.log("subtração", subtração)
+
+
+/////////////////////////////////////////////////////////////4. PARA EXIBIR ALGUMA INFORMAÇÃO NA TELA USA-SE:
+var textoa = "Olá,"
+var textob = "Eu sou o Lucas"
+var textoc = " "
+var resultadoA = textoa + textoc + textob
+console.log(resultadoA) 
+
+
+
+//ATIVIDADE PARA PRATICAR 1
+var nome = "Lucas"
+var sobrenome = "Leite"
+var ano = 2024
+var idade = 23
+var espaço = " "
+
+
+/*
+var FullName = nome + espaço + sobrenome
+console.log(FullName)
+
+var AnoNasceu = ano - idade
+console.log(AnoNasceu)
+
+
+
+///////////////////////////////////////////////////////////5. PROMPT e ALERT:
+alert("Seja Bem-Vindo")
+var NomeInserido = prompt("insira seu nome:") 
+console.log (NomeInserido)
+
+var entrada = prompt("insira um número")
+var saida = "Obrigado! O número" + " " + entrada + " " + "foi adicionado com sucesso"
+alert(saida)
+
+var valorString = prompt("insira um número")
+var valorConvertido =parseInt(valorString)
+console.log(valorConvertido)*/
+
+
+
+//ATIVIDADE PARA PRATICAR 2
+/*var primeiroNumero = prompt ("insira um número")
+var primeiraConversão = parseInt(primeiroNumero)
+var segundoNumero = prompt ("insira mais um número")
+var segundaConversão = parseInt(segundoNumero)
+var resultado = primeiraConversão * segundaConversão
+console.log (resultado)
+
+var primeiroNumero = prompt ("insira um número")
+var primeiraConversão = parseFloat(primeiroNumero)
+var segundoNumero = prompt ("insira mais um número")
+var segundaConversão = parseFloat(segundoNumero)
+var resultado = primeiraConversão * segundaConversão
+console.log (resultado)*/
+
+
+
+
